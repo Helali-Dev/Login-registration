6c816dad694c44eb0c120f9b2716f8ac63aaa75c
Fateme Helali <FatemeHelali2005@gmail.com>
2026-08-08 15:31:54 +0800

    feat: add login form switching

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // Elements
-const loginButton=document.querySelector(".login-btn");
-const registerButton=document.querySelector(".register-btn");
-const formTitle=document.querySelector(".form-title");
-const registerForm=document.querySelector(".register-form");
-const loginForm=document.querySelector(".login-form");
+const loginButton = document.querySelector(".login-btn");
+const registerButton = document.querySelector(".register-btn");
+const formTitle = document.querySelector(".form-title");
+const registerForm = document.querySelector(".register-form");
+const loginForm = document.querySelector(".login-form");
 
 // Show Register Form When We Click on the Register Button
-registerButton.addEventListener("click",()=>
-{
+registerButton.addEventListener("click", () => {
   // Change the Title of Form
-  formTitle.innerText="Register Form";
+  formTitle.innerText = "Register Form";
 
   //Toggle Form Buttons
   registerButton.classList.add("active");
@@ -20,3 +19,16 @@ registerButton.addEventListener("click",()=>
   loginForm.classList.remove("active");
 });
 
+// Show Login Form When We Click on the Login Button
+loginButton.addEventListener("click", () => {
+  // Change the Title of Form
+  formTitle.innerText = "Login Form";
+
+  //Toggle Form Buttons
+  registerButton.classList.remove("active");
+  loginButton.classList.add("active");
+
+  // Show Login Form and Hide Register Form
+  registerForm.classList.remove("active");
+  loginForm.classList.add("active");
+});
