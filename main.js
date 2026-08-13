b00ecb732f23a01c972b1c479bbc440eb82f2e8d
Fateme Helali <FatemeHelali2005@gmail.com>
2026-08-13 14:12:19 +0330

    feat : add register email,password and confirm password validation

--- main.js
+++ main.js
@@ -12,6 +12,22 @@ const loginEmailInput = document.getElementById("login-email-input");
 const loginEmailAlert = document.getElementById("login-email-alert");
 const loginPasswordInput = document.getElementById("login-password-input");
 const loginPasswordAlert = document.getElementById("login-password-alert");
+
+// Register Elements
+const registerEmailInput = document.getElementById("register-email-input");
+const registerEmailAlert = document.getElementById("register-email-alert");
+const registerPasswordAlert = document.getElementById(
+  "register-password-alert"
+);
+const registerPasswordInput = document.getElementById(
+  "register-password-input"
+);
+const registerConfirmPasswordInput = document.getElementById(
+  "register-confirm-password-input"
+);
+const registerConfirmPasswordAlert = document.getElementById(
+  "register-confirm-password-alert"
+);
 // Show Register Form When We Click on the Register Button
 registerButton.addEventListener("click", () => {
   // Change the Title of Form
@@ -123,3 +139,92 @@ const validateLoginForm = () => {
   }
   return true; // If Inputs Was Correct
 };
+
+// Validate Register Form
+registerForm.addEventListener("submit", (e) => {
+  if (!validateRegisterForm()) {
+    e.preventDefault();
+  }
+});
+// Validate Register Email
+registerEmailInput.addEventListener("input", () => {
+  const emailFormat =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!registerEmailInput.value.match(emailFormat)) {
+    registerEmailInput.classList.add("wrong-input");
+    registerEmailAlert.innerText = "Enter valid Email baby !";
+  } else {
+    registerEmailInput.classList.remove("wrong-input");
+    registerEmailAlert.innerText = "";
+  }
+});
+
+// Validate Register Pssword
+registerPasswordInput.addEventListener("input", () => {
+  if (registerPasswordInput.value.length < 6) {
+    registerPasswordInput.classList.add("wrong-input");
+    registerPasswordAlert.innerText = "Enter more than 6 characters !";
+  } else {
+    registerPasswordInput.classList.remove("wrong-input");
+    registerPasswordAlert.innerText = "";
+  }
+});
+
+// Validate Register Confirm Password
+registerConfirmPasswordInput.addEventListener("input", () => {
+  if (registerConfirmPasswordInput.value.length < 6) {
+    registerConfirmPasswordInput.classList.add("wrong-input");
+    registerConfirmPasswordAlert.innerText = "Enter more than 6 characters !";
+  } else if (
+    registerConfirmPasswordInput.value !== registerPasswordInput.value
+  ) {
+    registerConfirmPasswordInput.classList.remove("wrong-input");
+    registerConfirmPasswordAlert.innerText = "Password dosn't match";
+  } else {
+    registerConfirmPasswordInput.classList.remove("wrong-input");
+    registerConfirmPasswordAlert.innerText = "";
+  }
+});
+
+// Validate Register Form
+const validateRegisterForm = () => {
+  // Validate Email
+  const emailFormat =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!registerEmailInput.value.match(emailFormat)) {
+    registerEmailInput.classList.add("wrong-input");
+    registerEmailAlert.innerText = "Enter valid Email baby !";
+    return false;
+  }
+
+  // Validate Password
+  if (registerConfirmPasswordInput.value.length < 6) {
+    registerConfirmPasswordInput.classList.add("wrong-input");
+    registerConfirmPasswordAlert.innerText = "Enter more than 6 characters !";
+    return false;
+  } else {
+    registerConfirmPasswordInput.classList.remove("wrong-input");
+    registerConfirmPasswordAlert.innerText = "";
+  }
+
+  // Validate Confirm Password
+  if (registerConfirmPasswordInput.value.length < 6) {
+    registerConfirmPasswordInput.classList.add("wrong-input");
+    registerConfirmPasswordAlert.innerText = "Enter more than 6 characters !";
+    return false;
+  } else if (
+    registerConfirmPasswordInput.value !== registerPasswordInput.value
+  ) {
+    registerConfirmPasswordInput.classList.remove("wrong-input");
+    registerConfirmPasswordAlert.innerText = "Password dosn't match";
+    return false;
+  }
+
+  return true;  // if inputs true
+};
+
+
+
+
